45f5ea6236d5f648f0f1fc0c26d80ad7e5647306
Moet <moetodonnell@gmail.com>
2023-11-28 19:18:38 -0500

    added createvenue and login

--- app/src/pages/CreateVenue.js
+++ app/src/pages/CreateVenue.js
@@ -1,5 +1,85 @@
 import React, { useState } from 'react';
+
 export default function CreateVenue(){
+    const [LRows, setLRows] = useState('');
+    const [LCols, setLCols] = useState('');
+    const [CRows, setCRows] = useState('');
+    const [CCols, setCCols] = useState('');
+    const [RRows, setRRows] = useState('');
+    const [RCols, setRCols] = useState('');
+
+    const [LSeats, setLSeats] = useState('');
+    const [CSeats, setCSeats] = useState('');
+    const [RSeats, setRSeats] = useState('');
+
+
+    const [venueName, setVenueName] = useState('');
+
+    const handleClick = async (event, side) => {
+
+        let block = [];
+        if(side === 'L'){
+            for (let i = 0; i < parseInt(LRows); i++){
+                for(let n=0; n< parseInt(LCols); n++){
+                    block.push("☐");
+                }
+                block.push(<br/>);
+            }
+            setLSeats(block)
+        } else if(side === 'C'){
+            for (let i = 0; i < parseInt(CRows); i++){
+                for(let n=0; n< parseInt(CCols); n++){
+                    block.push("☐");
+                }
+                block.push(<br/>);
+            }
+            setCSeats(block)
+        } else if(side === 'R'){
+            for (let i = 0; i < parseInt(RRows); i++){
+                for(let n=0; n< parseInt(RCols); n++){
+                    block.push("☐");
+                }
+                block.push(<br/>);
+            }
+            setRSeats(block)
+        }
+    };
+    const saveVenue = async () => {
+
+        const payload = {
+            "venueName": venueName,
+            "layout": {
+                "center": [
+                    CRows,
+                    CCols
+                ],
+                "left": [
+                    LRows,
+                    LCols
+                ],
+                "right": [
+                    RRows,
+                    RCols
+                ]
+            }
+          };
+        /*
+        try {
+        const response = await fetch('', {
+            method: 'POST',
+            body: JSON.stringify(payload),
+        });
+    
+        const resultData = await response.json();
+        console.log(resultData);
+
+        
+        } catch (error) {
+        console.error('Error fetching data:', error);
+        }
+        */
+    }
+
     return (
         <div>
             <center>
@@ -8,11 +88,79 @@ export default function CreateVenue(){
                 <text>Venue Name: </text>
                 <input
                 id="venueName"
+                value={venueName} 
+                onChange={(e) => setVenueName(e.target.value)}
                 />
-                <br /><br />
-                <input type="button" value="Cancel & Exit Venue" />
-                <input type="button" value="Save & Exit Venue" />
+                <br/><br/>
+                <div class="row">
+                </div>
+                <a href= "/Home"><input type="button" value="Cancel and Exit Venue" /></a>
+                <a href= "/Home"><input type="button" value="Save and Exit Venue" onClick={saveVenue}/></a>
             </center>
+            <style
+                dangerouslySetInnerHTML={{
+                __html:
+                    '\n* {\n  box-sizing: border-box;\n}\n\n\n.column {\n  float: left;\n  width: 30%;\n  padding: 10px;\n \n}\n\n\n.row:after {\n  content: "";\n  display: table;\n  clear: both;\n}\n'
+                }}
+            />
+                <div className="column" style={{ backgroundColor: "#fff" }}>
+                    <center>
+                    <br/><br/><br/><br/><br/><br/><br/><br/>
+
+                    <text style={{ color: 'red' }}>* </text><text>Side Left</text>
+                    <br/><br />
+                    <input style={{width: "50px"}} placeholder="Rows" value={LRows} onChange={(e) => setLRows(e.target.value)} id="sideLeftRows"/>
+                    <text> x </text>
+                    <input style={{width: "50px"}} placeholder="Cols" value={LCols} onChange={(e) => setLCols(e.target.value)} id="sideLeftColumns"/>
+                    <text> </text><input type="button" value="APPLY" onClick={(e) => handleClick(e, 'L')} />
+                    
+                    <br /><br />
+
+                    <text style={{ color: 'red' }}>*</text><text >Center</text>
+                    <br/><br />
+                    <input style={{width: "50px"}} placeholder="Rows" value={CRows} onChange={(e) => setCRows(e.target.value)} id="CenterRows"/>
+                    <text> x </text>
+                    <input style={{width: "50px"}} placeholder="Cols" value={CCols} onChange={(e) => setCCols(e.target.value)} id="CenterColumns"/>
+                    <text> </text><input type="button" value="APPLY" onClick={(e) => handleClick(e, 'C')} />
+                    <br/>
+                    <br /><br />
+
+                    <text style={{ color: 'red' }}>*</text><text> Side Right</text>
+                    <br/><br />
+                    <input style={{width: "50px"}} placeholder="Rows" value={RRows} onChange={(e) => setRRows(e.target.value)} id="sideRightRows"/>
+                    <text> x </text>
+                    <input style={{width: "50px"}} placeholder="Cols" value={RCols} onChange={(e) => setRCols(e.target.value)} id="sideRightColumns"/>
+                    <text> </text><input type="button" value="APPLY" onClick={(e) => handleClick(e, 'R')} />
+                    <br/>
+                    
+                    </center>
+                </div>
+                
+                <div className="column" style={{ backgroundColor: "#fff" }}>
+                    <center>
+                    <h1>Stage</h1>
+                    <style
+                    dangerouslySetInnerHTML={{
+                    __html:
+                                    '\n* {\n  box-sizing: border-box;\n}\n\n\n.column {\n  float: left;\n  width: 33.33%;\n  padding: 10px;\n /\n}\n\n\n.row:after {\n  content: "";\n  display: table;\n  clear: both;\n}\n'
+                                }}
+                    />
+                    <div className="row">
+                        <div className="column" style={{ backgroundColor: "#fff" }}>
+                            <h3>Side Left</h3>
+                            <h3>{LSeats}</h3>
+                        </div>
+                        <div className="column" style={{ backgroundColor: "#fff" }}>
+                            <h3>Center</h3>
+                            <h3>{CSeats}</h3>
+                        </div>
+                        <div className="column" style={{ backgroundColor: "#fff" }}>
+                            <h3>Side Left</h3>
+                            <h3>{RSeats}</h3>
+                        </div>
+                    </div>
+                    </center>
+                </div>
         </div>
     )
 }
--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,5 +1,30 @@
 import React, { useState } from 'react';
 export default function Login(){
+
+    const [password, setPassword] = useState('');
+    let authorized = false;
+
+    const handleClick = async () => {
+        const payload = {
+          authentification: password,
+        };
+        /*
+        try {
+          const response = await fetch('', {
+            method: 'GET',
+            body: JSON.stringify(payload),
+          });
+    
+          const result = await response.json();
+          if (result === 200){
+            authorized == true;
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+        */
+      };
+
     return (
         <div>
             <center>
@@ -9,9 +34,11 @@ export default function Login(){
                 <input
                 id="password"
                 placeholder='PASSWORD'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 />
                 <br /><br />
-                <input type="button" value="LOG IN" />
+                <input type="button" value="LOG IN" onClick={handleClick} />
             </center>
         </div>
     )
