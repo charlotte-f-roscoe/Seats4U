8082788044a393b443d3ac50403deb3b6d87cb04
Moet <moetodonnell@gmail.com>
2023-11-29 15:02:42 -0500

    edited app.js

--- app/src/App.js
+++ app/src/App.js
@@ -5,31 +5,41 @@ import CreateVenue from "./pages/CreateVenue.js";
 import ErrorNotFound from "./pages/ErrorNotFound.js";
 import CreateShow from './pages/CreateShow.js';
 import ListVenues from './pages/ListVenues.js';
+import DeleteVenue from './pages/DeleteVenue.js';
 
 
 const Seats4U = () => {
-  let component
-  switch (window.location.pathname){
-    case "/":
-        component = <Home />
-        break
-    case "/Home":
-        component = <Home />
-        break
-    case "/Login":
-        component = <Login />
-        break
-    case "/CreateVenue":
-        component = <CreateVenue />
-        break
-    case "/CreateShow":
-        component = <CreateShow />
-        break
-    case "/ListVenues":    
-        component = <ListVenues />
-        break
-    default:
-        component = <ErrorNotFound />
+
+    const [component, setComponent] = useState(<Home />);
+
+    const handleClick = async (event, number) => {
+
+        switch (number){
+
+            case 0:
+                setComponent(<Home />)
+                break
+            case 1:
+                setComponent(<Home />)
+                break
+            case 2:
+                setComponent(<Login />)
+                break
+            case 3:
+                setComponent(<CreateVenue />)
+                break
+            case 4:
+                setComponent(<CreateShow />)
+                break
+            case 5:    
+                setComponent(<ListVenues />)
+                break
+            case 6:
+                setComponent(<DeleteVenue />)
+                break
+            default:
+                setComponent(<Home />)
+        }
 
   }
 
@@ -38,10 +48,12 @@ const Seats4U = () => {
     <div>
         <center>
             <h1></h1>
-            <a href= "/Home"><button>HOME</button></a>
-            <a href= "/CreateShow"><button>CREATE SHOW</button></a>
-            <a href= "/CreateVenue"><button>CREATE VENUE</button></a>
-            <a href= "/ListVenues"><button>LIST VENUES</button></a>
+            <input type="button" value="HOME" onClick={(e) => handleClick(e, 0)} />
+            <input type="button" value="CREATE SHOW" onClick={(e) => handleClick(e, 4)} />
+            <input type="button" value="CREATE VENUE" onClick={(e) => handleClick(e, 3)} />
+            <input type="button" value="LIST VENUES" onClick={(e) => handleClick(e, 5)} />
+            <input type="button" value="DELETE VENUE" onClick={(e) => handleClick(e, 6)} />
+
         </center>
         {component}
     </div>
