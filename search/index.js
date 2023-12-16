3f9dd3460f7abb2d7d5d6489fdefccb636b58ac9
MurkingtonWizard <ejkrum@yahoo.com>
2023-12-15 23:03:57 -0500

    Updated date/time information

--- search/index.js
+++ search/index.js
@@ -4,47 +4,66 @@ const db_access = require('/opt/nodejs/db_access');
 exports.handler = async (event) => {
   // get credentials from the db_access layer (loaded separately via AWS console)
   var pool = mysql.createPool({
-      host: db_access.config.host,
-      user: db_access.config.user,
-      password: db_access.config.password,
-      database: db_access.config.database
+    host: db_access.config.host,
+    user: db_access.config.user,
+    password: db_access.config.password,
+    database: db_access.config.database
   });
   let SearchDB = (value) => {
-    value = "\%"+value+"\%";
+    value = "\%" + value + "\%";
     return new Promise((resolve, reject) => {
-       pool.query("SELECT * FROM Shows WHERE showName LIKE ? OR venueName LIKE ? OR showDate LIKE ? order by venueName, showDate, startTime", [1,value,value,value], (error, rows) => {
+      pool.query("SELECT * FROM Shows WHERE showName LIKE ? OR venueName LIKE ? OR showDate LIKE ? order by venueName, showDate, startTime", [value, value, value], (error, rows) => {
         if (error) { return reject(error); }
         return resolve(rows);
       });
     });
   };
-  let updateActiveShows = () => {
-    return new Promise((resolve,reject) => {
-      let curr = new Date();
-      let timeStr = `${curr.getHours()}${curr.getMinutes()}`;
-      pool.query("UPDATE Shows SET active=? WHERE (active=?) AND ((showDate=? AND startTime<=?) OR showDate<?)",[0,1,curr,timeStr,curr],(error,rows) => {
-        if(error) { return reject(error); }
+  let GetActiveShows = () => {
+    return new Promise((resolve, reject) => {
+      pool.query("SELECT * FROM Shows WHERE active=?", [1], (error, rows) => {
+        if (error) { return reject(error); }
         return resolve(rows);
       });
     });
   };
-  
+  let updateActiveShows = (showID) => {
+    return new Promise((resolve, reject) => {
+      pool.query("UPDATE Shows SET active=? WHERE showID=?", [0, showID], (error, rows) => {
+        if (error) { return reject(error); }
+        return resolve(rows);
+      });
+    });
+  };
+
   let response = undefined;
-  
+
   try {
-    await updateActiveShows();
+    let activeShows = await GetActiveShows();
+    const timeZone = "America/New_York";
+    let curr = new Date(new Date().toLocaleString('en-US', { timeZone }));
+    let timeStr = `${curr.getHours()}${curr.getMinutes()}`;
+    curr.setHours(0, 0, 0, 0);
+    for(let i=0;i<activeShows.length;i++) {
+      let showDate = activeShows[i].showDate;
+      let isInFuture = showDate > curr || (showDate.getYear() == curr.getYear() && showDate.getMonth() == curr.getMonth() && showDate.getDate() == curr.getDate() && activeShows[i].startTime >= timeStr);
+      if(!isInFuture) {
+        await updateActiveShows(activeShows[i].showID);
+      }
+    }
     let results = await SearchDB(event.search);
-    
+
     response = {
       statusCode: 200,
       shows: results
     };
-  } catch (err) {
+  }
+  catch (err) {
     response = {
       statusCode: 400,
       error: err
     };
-  } finally {
+  }
+  finally {
     pool.end();
   }
   return response;
